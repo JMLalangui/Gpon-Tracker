ca6851607f50d4efdafbfe1391427cb85a5c7197
JMLalangui <jlalangui@tecnologicoismac.edu.ec>
2024-09-22 12:09:25 -0500

    JL-T-V00001-edicion plantilla AdminTLE -- se deja lo mas simple posible pendiente acabar

--- index.php
+++ index.php
@@ -6,6 +6,14 @@ include ('app/config.php');
 session_start();
 if(isset($_SESSION['session_email'])){
   $email_sesion = $_SESSION['session_email'];
+  $sql = "SELECT nombres FROM gpon.users WHERE email = '$email_sesion'";
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+
+  foreach($usuarios as $usuario){
+      $nombres_session = $usuario['nombres'];
+}
 }else{
   header('Location: '.$URL.'login/');
 }
@@ -35,6 +43,7 @@ VALIDACION DE SEGURIDAD DE ACCESO A SISTEMA MEDIANTE EL USO DE SESSIONES
   <!--Libreria Sweet Alert2-->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body class="hold-transition sidebar-mini">
 <script>
   Swal.fire({
@@ -47,173 +56,109 @@ VALIDACION DE SEGURIDAD DE ACCESO A SISTEMA MEDIANTE EL USO DE SESSIONES
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Gpon tracker</a>
-      </li>
-    </ul>
-
-    <!-- Right navbar links -->
+    
+    <!--============================================ NOMBRE SISTEMA EN NAV BAR ============================================-->
+      <ul class="navbar-nav">
+          <li class="nav-item d-none d-sm-inline-block">
+            <a href="#" class="nav-link">Gpon tracker</a>
+          </li>
+      </ul>
+    <!--============================================ NOMBRE SISTEMA EN NAV BAR ============================================-->      
+    <!--============================================ TABLA DE NOTAS PERSONALIZADAS ============================================-->
     <ul class="navbar-nav ml-auto">
-
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="public/templates/AdminLTE/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="public/templates/AdminLTE/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="public/templates/AdminLTE/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li>
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
           <i class="fas fa-th-large"></i>
         </a>
       </li>
     </ul>
+    <!--============================================ TABLA DE NOTAS PERSONALIZADAS ============================================-->
   </nav>
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <!--============================================ SECCION DE LOGO ============================================-->
+    <a href="<?php echo $URL?>" class="brand-link">
       <img src="<?php echo $URL?>public/images/gpon-tracker-icon.ico" alt="Gpon Tracker Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Gpon tracker</span>
     </a>
+    <!--============================================ SECCION DE LOGO ============================================-->
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
+      <!--============================================ IMAGEN Y NOMBRE DE USUARIO (optional) ============================================-->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="public/templates/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="public/templates/AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> <!-- PENDIENTE EDITAR IMAGEN-->
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?php echo $nombres_session?></a>
         </div>
       </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      <!--============================================ IMAGEN Y NOMBRE DE USUARIO (optional) ============================================-->
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+          <!--============================================ OPCION DE MENU CON ITEM DESPLEGABLES ============================================-->
+          <li class="nav-item">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
+                MONITOREO 
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                  <p>OLT</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>EQUIPOS</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>TRAFICO</p>
                 </a>
               </li>
             </ul>
           </li>
+          <!--============================================ OPCION DE MENU CON ITEM DESPLEGABLES ============================================-->
+          <!--============================================ OPCION DE MENU CON ITEM DESPLEGABLES MANTENIMIENTO ============================================-->
+          <li class="nav-item">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-cog"></i>
+              <p>
+                MANTENIMIENTO
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>USUARIOS</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>EQUIPOS</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <!--============================================ OPCION DE MENU CON ITEM DESPLEGABLES MANTENIMIENTO ============================================-->
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
@@ -223,6 +168,7 @@ VALIDACION DE SEGURIDAD DE ACCESO A SISTEMA MEDIANTE EL USO DE SESSIONES
               </p>
             </a>
           </li>
+        <!--============================================ CERRARA SESION ============================================-->
           <li class="nav-item">
           <a href="<?php echo $URL;?>/app/controllers/login/cerrar_sesion.php" class="nav-link">
             <i class="far fa-circle text-danger"></i>
@@ -231,6 +177,7 @@ VALIDACION DE SEGURIDAD DE ACCESO A SISTEMA MEDIANTE EL USO DE SESSIONES
             </p>
           </a>
           </li>
+        <!--============================================ CERRARA SESION ============================================-->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
@@ -245,13 +192,7 @@ VALIDACION DE SEGURIDAD DE ACCESO A SISTEMA MEDIANTE EL USO DE SESSIONES
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
+            <h1 class="m-0">SISTEMA DE MONITOREO DE RED</h1>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
